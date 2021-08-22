0ddfda86aa95b09659a9c6e9d3aa8099e1a84d91
BasemTrad <tradbasem@gmail.com>
2021-08-22 21:59:25 +0300

    sign up & log in & router

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./login";
 
 function App() {
+  function sign() {
+    window.location.href = "/login"
+    
+  }
+  function signup()  {
+    window.location.href = "/signup";
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login">
+         <Login />
+        </Route>
+        <Route path="/signup"><div>
+          <h1>signup</h1></div> </Route>
+
+          <Route path="/">
+            <div>
+              <h1>Wlecom</h1>
+              <button onClick={sign}>log in</button>
+              <button onClick={signup}>sign up</button>
+            </div>
+          </Route>
+          
+      </Switch>
+    </Router>
   );
 }
 
